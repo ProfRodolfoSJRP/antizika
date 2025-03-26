fe3e0cb27ba139697a0259463f0284eba1f6d506
ProfRodolfoSJRP <163571628+ProfRodolfoSJRP@users.noreply.github.com>
2025-03-26 16:59:04 -0300

    tela dialogo

--- antiZika/antiZika/Form1.cs
+++ antiZika/antiZika/Form1.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 namespace antiZika
 {
     public partial class Form1 : Form
@@ -9,7 +11,35 @@ namespace antiZika
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Abrir Windows Explorer ( File Dialog )
+            using (OpenFileDialog dialogoAbrirArquivo = new OpenFileDialog()) 
+            {
+                dialogoAbrirArquivo.Filter = "Todos os arquivos (*.*)|*.*";
 
+                if(dialogoAbrirArquivo.ShowDialog() == DialogResult.OK)
+                {
+                    // Armazena o caminho do arquivo
+                    string caminhoArquivo = dialogoAbrirArquivo.FileName;
+                    
+                    //Calcula a hash
+                    string hashSHA256 = CalculaHashSHA256(caminhoArquivo);
+                    Clipboard.SetText(hashSHA256);
+                    MessageBox.Show("Hash Colado na area de transferencia");
+                }
+
+            }
+        }
+
+        // Calcular Hash SHA256
+        private string CalculaHashSHA256(string caminhoArquivo)
+        {
+            using (FileStream fs = File.OpenRead(caminhoArquivo))// Abre o arquivo
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Computa o hash do arquivo e converter os bytes para string
+                byte[] hashBytes = sha256.ComputeHash(fs);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
         }
     }
 }
