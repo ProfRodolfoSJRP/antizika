f88a01257ebc5dbac74cbfae610dac06dba5d1f1
ProfRodolfoSJRP <163571628+ProfRodolfoSJRP@users.noreply.github.com>
2025-03-26 15:19:03 -0300

    Botões

--- antiZika/antiZika/Form1.Designer.cs
+++ antiZika/antiZika/Form1.Designer.cs
@@ -29,22 +29,93 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.IndianRed;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(22, 49);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 146);
+            button1.TabIndex = 0;
+            button1.Text = "Calcular Hash";
+            button1.TextAlign = ContentAlignment.BottomLeft;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Goldenrod;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(198, 49);
+            button2.Name = "button2";
+            button2.Size = new Size(139, 146);
+            button2.TabIndex = 1;
+            button2.Text = "Escanear";
+            button2.TextAlign = ContentAlignment.BottomLeft;
+            button2.UseVisualStyleBackColor = false;
+           // 
+            // button3
+            // 
+            button3.BackColor = Color.DarkOliveGreen;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(381, 49);
+            button3.Name = "button3";
+            button3.Size = new Size(139, 146);
+            button3.TabIndex = 2;
+            button3.Text = "Escanear Pastas";
+            button3.TextAlign = ContentAlignment.BottomLeft;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(229, 244);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 15);
+            label1.TabIndex = 3;
+            label1.Text = "AntiVirus Zika";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            ClientSize = new Size(500, 268);
+            BackColor = SystemColors.InactiveBorder;
+            ClientSize = new Size(536, 268);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            MdiChildrenMinimizedAnchorBottom = false;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "AntiZika";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
     }
 }
--- antiZika/antiZika/Form1.cs
+++ antiZika/antiZika/Form1.cs
@@ -6,5 +6,10 @@ namespace antiZika
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
